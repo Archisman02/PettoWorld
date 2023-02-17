9ebdcdc7bacdd6d3918062521ea4632c03c8bf37
Archisman <archisman.mukherjee02@gmail.com>
2023-02-17 21:12:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const port = 8000;
+// const bodyParser = require("body-parser");
+const app = express();
+
+// app.get("/", function (req, res) {
+//   res.send("<h1>Running</h1>");
+// });
+
+// app.use(
+//   bodyParser.urlencoded({
+//     // This is a middleware
+//     extended: true,
+//   })
+// );
+
+// use express router
+app.use("/", require("./routes/index"));
+
+app.listen(port, function (err) {
+  if (err) {
+    // console.log("Error: ", err);
+    console.log(`Error in running the server : ${port}`);
+  }
+
+  console.log(`Server is running on port: ${port}`);
+});
